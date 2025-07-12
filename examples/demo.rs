66fd3c2913320452d3ba774c244ccbdf3640fb99
Aleksey Kliger <aleksey@lambdageek.org>
2025-07-12 01:59:09 -0400

    naive async_server and demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,11 +1,40 @@
-#[cfg(false)]
-use friendly_pipes::{async_server, consumer};
+use friendly_pipes::{async_server, producer};
+
+const ENV: &str = "PIPE_PATH";
 
 fn main() -> std::process::ExitCode {
     if std::env::args().count() < 2 {
         println!("server");
+        let base_dir = std::path::Path::new("/tmp/friendly_pipe");
+        std::fs::create_dir_all(base_dir).expect("Failed to create base directory");
+        let path = base_dir.join(format!("{pid}.sock", pid = std::process::id()));
+        let on_line = |line: &[u8]| {
+            println!("Received line: {}", String::from_utf8_lossy(line));
+        };
+        tokio::runtime::Runtime::new()
+            .expect("Failed to create Tokio runtime")
+            .block_on(async {
+                let current_exe =
+                    std::env::current_exe().expect("Failed to get current executable path");
+                let server = async_server::start(path.as_os_str(), on_line);
+                let mut child = std::process::Command::new(current_exe)
+                    .env(ENV, path.as_os_str())
+                    .arg("client")
+                    .spawn()
+                    .expect("Failed to start client");
+                println!("Client started with PID: {}", child.id());
+                child.wait().expect("Failed to wait for client process");
+                server.stop();
+            });
     } else if std::env::args().nth(1).is_some_and(|arg| arg == "client") {
         println!("client");
+        let path = std::env::var_os(ENV).expect("PIPE_PATH environment variable not set");
+        let mut client = producer::Producer::new(&path).expect("Failed to create producer");
+        let msg = format!("Hello from client {pid}!\n", pid = std::process::id());
+        client
+            .write(msg.as_bytes())
+            .expect("Failed to write to pipe");
+        println!("Message sent to server.");
     } else {
         eprintln!("Usage: {} [client]", std::env::args().next().unwrap());
         return std::process::ExitCode::FAILURE;
--- src/async_server.rs
+++ src/async_server.rs
@@ -1,1 +1,89 @@
+use std::ffi::OsStr;
 
+use crate::consumer::Consumer;
+
+use tokio::io::AsyncReadExt;
+use tokio::select;
+use tokio::task;
+use tokio_stream::StreamExt;
+use tokio_util::sync::CancellationToken;
+
+pub async fn start_full<F>(
+    path: &OsStr,
+    finish: CancellationToken,
+    on_line: F,
+) -> std::io::Result<()>
+where
+    F: Fn(&[u8]) + Send + Sync + 'static,
+{
+    let on_line = std::sync::Arc::new(on_line);
+    let mut consumer = Consumer::new(path)?;
+    loop {
+        select! {
+            _ = finish.cancelled() => {
+                println!("Server stopped by cancellation token.");
+                break;
+            }
+            it = consumer.next() => {
+                match it {
+                    Some(Ok(mut client)) => {
+                        let on_line = on_line.clone();
+                        task::spawn(async move {
+                        let mut buffer = vec![0; 1024];
+                        loop {
+                            match client.read(&mut buffer).await {
+                                Ok(0) => {
+                                    println!("Client disconnected.");
+                                    break;
+                                }
+                                Ok(n) => {
+                                    on_line(&buffer[..n]);
+                                }
+                                Err(e) => {
+                                    eprintln!("Error reading from client: {e}");
+                                    break;
+                                }
+                            }
+                        }
+                    });
+                },
+                Some(Err(e)) => {
+                    eprintln!("Error accepting client: {e}");
+                    break;
+                }
+                None => {
+                    println!("No more clients connected.");
+                    break;
+                }
+            }
+            }
+        }
+    }
+    Ok(())
+}
+
+pub struct AsyncServer {
+    cancel: CancellationToken,
+}
+
+pub fn start<F>(path: &OsStr, on_line: F) -> AsyncServer
+where
+    F: Fn(&[u8]) + Send + Sync + 'static,
+{
+    let cancel = CancellationToken::new();
+
+    let finish = cancel.clone();
+    let path = path.to_owned();
+
+    tokio::spawn(async move {
+        start_full(&path, finish, on_line).await.unwrap();
+    });
+
+    AsyncServer { cancel }
+}
+
+impl AsyncServer {
+    pub fn stop(&self) {
+        self.cancel.cancel();
+    }
+}
